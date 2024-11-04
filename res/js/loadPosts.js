02c57c294166ffa1491c1e90ea774dcef9135004
Anyro <144886688+Anyro0@users.noreply.github.com>
2024-11-04 14:45:59 +0200

    Update loadPosts.js

--- res/js/loadPosts.js
+++ res/js/loadPosts.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('https://gist.githubusercontent.com/Anyro0/8b85110c4018e8be17c169915de66d8f/raw/7ebb7dda6b3fe702505c6836d8629cfd2145839b/posts.json')
+    //fetch('https://gist.githubusercontent.com/Anyro0/8b85110c4018e8be17c169915de66d8f/raw/7ebb7dda6b3fe702505c6836d8629cfd2145839b/posts.json')
+    fetch('res/data/posts.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error("Failed to fetch JSON data");
