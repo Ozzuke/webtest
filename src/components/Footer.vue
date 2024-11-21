cfa58789a275a3efbcbf2a47bd650cbb2f2b4399
Leo-Martin Pala <57061002+Leo-Martin-Pala@users.noreply.github.com>
2024-11-21 14:44:20 +0200

    created footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,9 +5,16 @@ export default {
 </script>
 
 <template>
-footer
+  <footer>
+    <p>©2024 Team X</p>
+  </footer>
 </template>
 
 <style scoped>
-
+footer {
+  flex-shrink: 0;
+  background-color: #f0f0f0;
+  padding: 10px;
+  text-align: center;
+}
 </style>
